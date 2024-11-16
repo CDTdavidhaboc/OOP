cf765fcf19193fc71da1b2dc8212c20ac8e4d8b6
CDTdavidhaboc <23-14890@g.batstate-u.edu.ph>
2024-11-16 08:54:50 +0800

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,22 @@
+class Person{
+    protected String Name;
+    protected String Address;
+
+    public void setName (String Name) {
+        this.Name = Name;
+
+}
+    public void setAddress (String Address) {
+        this.Address = Address; 
+}
+    public String getName() {
+        return Name;
+}
+    public String getAddress() {
+        return Address; 
+}
+@Override
+    public String toString(){
+        return "Hello I am" + Name + "and" + "I live in" + Address;
+}
+}
